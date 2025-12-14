4b7afe940b1e421c05fd8df7cb16b7a5afcaa49f
victorspio <depositoserradofelix@gmail.com>
2025-12-13 22:11:42 -0300

    Fix GitHub Pages routing and disable service worker

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import App from './App';
 import './styles/globals.css';
-import { registerServiceWorker } from './utils/serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/deposito-serra-felix-amostra">
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
-
-// Registra o Service Worker para atualizações automáticas
-registerServiceWorker();
